9d4437940ef2353726050a07875e6b4f8a65edd0
Harsh <87485552+Itstudentjnd@users.noreply.github.com>
2025-02-12 22:30:43 +0530

    Update settings.py

--- csvtopdf/settings.py
+++ csvtopdf/settings.py
@@ -82,8 +82,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'shivrati_netram',          # Database name
-        'USER': 'root',            # MySQL username
-        'PASSWORD': '',  # MySQL password
+        'USER': 'shivrati_netram',            # MySQL username
+        'PASSWORD': 'junagadhpolice@123',  # MySQL password
         'HOST': 'localhost',
         'PORT': '3306',
         'OPTIONS': {
