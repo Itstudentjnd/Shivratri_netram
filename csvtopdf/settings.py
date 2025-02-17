a684982a6b290097f1ddc51d3a904df054aacee2
Harsh <87485552+Itstudentjnd@users.noreply.github.com>
2025-02-17 12:50:55 +0530

    Update settings.py

--- csvtopdf/settings.py
+++ csvtopdf/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'pdfapp.middleware.auto_logout.AutoLogoutMiddleware',
 ]
 
 ROOT_URLCONF = 'csvtopdf.urls'
